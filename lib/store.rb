44b88e732c15b41cd327165404741413a601cb31
Connie Ho <connie3@ualberta.ca>
2021-03-29 19:40:34 +0000

    add ex9

--- lib/store.rb
+++ lib/store.rb
@@ -1,6 +1,17 @@
 class Store < ActiveRecord::Base
   has_many :employees
-  
+
   validates :name, length: { minimum: 3}
   validates :annual_revenue, inclusion: { in: 0..Float::INFINITY }
+
+  before_destroy :check_num_employees
+
+  private 
+
+  def check_num_employees
+    if self.employees.count > 0
+      return false
+    end
+  end
+
 end
